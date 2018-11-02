274d3a1da6ebaa77b8c08398c534b605fb2f75e6
wanglin0413 <375952185@qq.com>
2018-11-02 13:43:35 +0800

    https://leetcode.com/problems/convert-bst-to-greater-tree/

---
+++ src/easy/GreaterTree.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class GreaterTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    int sum=0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if(root != null){
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+
+}
