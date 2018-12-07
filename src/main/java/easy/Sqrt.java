a90981bda04c2338c0f4121c269f126f974c54e8
wanglin0413 <375952185@qq.com>
2018-12-07 11:03:10 +0800

    https://leetcode.com/problems/sqrtx/

---
+++ src/main/java/easy/Sqrt.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class Sqrt {
+    public int mySqrt(int x) {
+        if(x == 0)return 0;
+        long begin =x;
+        while (begin* begin > x || (begin+1)*(begin+1) <=x){
+            begin = (x / begin + begin)/2;
+        }
+        return (int) begin;
+    }
+
+    public static void main(String[] args) {
+        Sqrt sqrt = new Sqrt();
+        System.out.println(sqrt.mySqrt(2147395600));
+    }
+}
