41993ccddf5e8106f158507dbcf2a75b6a3173e5
wanglin0413 <375952185@qq.com>
2018-12-12 11:07:17 +0800

    https://leetcode.com/problems/path-sum/

---
+++ src/main/java/easy/BTPathSum.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class BTPathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)return false;
+        if(root.left == null && root.right == null)return sum == root.val;
+        sum -= root.val;
+        if(root.right ==null)return hasPathSum(root.left, sum);
+        if(root.left == null)return hasPathSum(root.right, sum);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+
+}
