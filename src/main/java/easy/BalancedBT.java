a0ca3c4f9eb18f6c777524a1eabf3828c7b04280
wanglin0413 <375952185@qq.com>
2018-12-12 10:49:12 +0800

    https://leetcode.com/problems/balanced-binary-tree/

---
+++ src/main/java/easy/BalancedBT.java
@@ -0,0 +1,21 @@
+package easy;
+
+
+public class BalancedBT {
+    //平衡树的定义：左右子树的高度差不大于1
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)return true;
+        int left = length(root.left);
+        int right = length(root.right);
+        int diff = Math.abs(left - right);
+        return diff<=1 && isBalanced(root.right) && isBalanced(root.left);
+    }
+
+    private int length(TreeNode root){
+        if(root == null)return 0;
+        return Math.max(length(root.left)+1, length(root.right)+1);
+    }
+
+
+
+}
