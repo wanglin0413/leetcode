66027829478a518d52d1b19d5c150885d31f7672
wanglin0413 <375952185@qq.com>
2018-12-18 16:04:32 +0800

    https://leetcode.com/problems/excel-sheet-column-title/

---
+++ src/main/java/easy/ExcelColumnTitle.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class ExcelColumnTitle {
+    public String convertToTitle(int n) {
+        //十进制转换为26进制
+        //ascii 大写字母A--65
+        StringBuilder sb = new StringBuilder();
+        while (n != 0){
+            int resual = n % 26;
+            if(resual == 0){
+                sb.append("Z");
+                n /=26;
+                n--;
+            }else {
+                sb.append((char)(resual+64));
+                n /= 26;
+            }
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ExcelColumnTitle ect = new ExcelColumnTitle();
+        System.out.println(ect.convertToTitle(27));
+    }
+}
