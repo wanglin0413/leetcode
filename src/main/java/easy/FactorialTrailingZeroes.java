bf1374af1ada1c41fd1198312d21898092ef9d2b
wanglin0413 <375952185@qq.com>
2018-12-19 09:55:53 +0800

    https://leetcode.com/problems/factorial-trailing-zeroes/

---
+++ src/main/java/easy/FactorialTrailingZeroes.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (n > 0){
+            n /= 5;
+            result += n;
+        }
+        return result;
+    }
+}
