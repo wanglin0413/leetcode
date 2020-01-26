f7cffa74370c21a895f470fe93cab14c86b01ea8
wanglin0413 <375952185@qq.com>
2020-01-26 12:20:42 +0800

    https://leetcode.com/problems/longest-valid-parentheses/

---
+++ src/main/java/hard/LongestParenthese.java
@@ -0,0 +1,58 @@
+package hard;
+
+import java.util.Stack;
+
+public class LongestParenthese {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+    public int longestValidParentheses1(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+
+    public static void main(String[] args) {
+        LongestParenthese lp = new LongestParenthese();
+        System.out.println(lp.longestValidParentheses("(()(((()"));
+    }
+}
