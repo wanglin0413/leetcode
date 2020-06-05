7326db32bf25071f1e4f5c0fe00edbae803f78d6
wanglin0413 <375952185@qq.com>
2020-06-05 16:53:53 +0800

    https://leetcode.com/problems/diameter-of-binary-tree/

---
+++ src/main/java/easy/DiameterOfBT.java
@@ -0,0 +1,29 @@
+package easy;
+
+import utils.MainClass;
+
+public class DiameterOfBT {
+    //求二叉树的直径
+    //直径定义：二叉树中任何两个节点间的最长path的长度，可以不经过root
+    private int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        postTraverse(root);
+        return max;
+    }
+
+    private int postTraverse(TreeNode root) {
+        if(root == null)return 0;
+        int left = postTraverse(root.left);
+        int right = postTraverse(root.right);
+        if(left+right > max){
+            max = left+right ;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = MainClass.stringToTreeNode("[1,2,3,4,5]");
+        DiameterOfBT dbt = new DiameterOfBT();
+        System.out.println(dbt.diameterOfBinaryTree(root));
+    }
+}
