c66ddfbfc14cb05e2274470afdd34930063933a1
wanglin0413 <375952185@qq.com>
2020-08-21 10:20:54 +0800

    https://leetcode.com/problems/binary-number-with-alternating-bits/

---
+++ src/main/java/easy/AlternateBits.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class AlternateBits {
+    public boolean hasAlternatingBits(int n) {
+        
+//        int i = n % 2;
+//        n = n/2;
+//        while(n != 0){
+//            int cur = n % 2;
+//            if(cur == i)return false;
+//            i = cur;
+//            n = n/2;
+//        }
+//        return true;
+
+        return (n & (n >> 1)) == 0 && (n | (n >> 2)) == n;
+    }
+
+    public static void main(String[] args) {
+        AlternateBits ab = new AlternateBits();
+        System.out.println(ab.hasAlternatingBits(5));
+    }
+}
