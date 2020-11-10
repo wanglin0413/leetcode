e9e3b03bb2f1006d69085453c0a59f2af115cab6
wanglin0413 <375952185@qq.com>
2020-11-10 13:44:32 +0800

    https://leetcode.com/problems/permutations/ https://leetcode.com/problems/permutations-ii/

--- src/main/java/medium/Permutations.java
+++ src/main/java/medium/Permutations.java
@@ -31,9 +31,44 @@ public class Permutations {
         }
     }
 
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> numList = new ArrayList<>();
+        for(int each : nums){
+            numList.add(each);
+        }
+        return permuteInner(numList);
+
+    }
+
+    private List<List<Integer>> permuteInner(List<Integer> nums) {
+        if(nums.size() == 1){
+            List<List<Integer>> oneNumList = new ArrayList<>();
+            oneNumList.add(nums);
+            return oneNumList;
+        }
+        List<List<Integer>> innerResult = new ArrayList<>();
+        Integer cur = null;
+
+        for(Integer each : nums){
+            if(!each.equals(cur)){
+                List<Integer> remained = new ArrayList<>(nums);
+                remained.remove(each);
+                List<List<Integer>> oneList = permuteInner(remained);
+                for(List<Integer> eachList : oneList){
+                    eachList.add(0, each);
+                    innerResult.add(eachList);
+                    cur = each;
+                }
+            }
+        }
+        return innerResult;
+    }
+
     public static void main(String[] args) {
         Permutations permutations = new Permutations();
-        int[] nums = new int[]{1,2,3};
-        System.out.println(permutations.permute(nums));
+        int[] nums = new int[]{1,1,2};
+//        System.out.println(permutations.permute(nums));
+        System.out.println(permutations.permuteUnique(nums));
     }
 }
